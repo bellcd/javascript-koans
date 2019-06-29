6659f8ec3668b2038c982cb468312f6a595022c3
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-06-29 03:44:22 -0400

    Fixes should find the 10001st prime test

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -108,7 +108,6 @@ describe("About Applying What We Have Learnt", function() {
     }
     return true;
   }
-  const PRIMES_UNDER_1000 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 183, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
   it("should find the largest prime factor of a composite number", function () {
     // assume: composite is a composite number
     function findLargestPrimeFactorOfComposite(composite) {
@@ -236,8 +235,33 @@ describe("About Applying What We Have Learnt", function() {
     expect(difference([-4, -2])).toBe(-16);
     expect(difference([1.5, -3])).toBe(9);
   });
-  //
-  // it("should find the 10001st prime", function () {
-  //
-  // });
+
+  it("should find the 10001st prime", function () {
+    const PRIMES_UNDER_1000 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 183, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
+    // assume
+      // indexN will be a integer greater than 0
+
+    // loop by 1, checking if each num is prime
+      // if yes, set currentPrime to num
+      // increment primeCount
+      // set index to current currentPrime
+    // stop when primeCount equals indexN
+    function findTheNthPrime(indexN) {
+      let num = 2, primeCount = 0, currentPrime;
+      while (primeCount !== indexN) {
+          if (isPrime(num)) {
+            currentPrime = num;
+            primeCount++;
+            num = currentPrime;
+          }
+          num++;
+      }
+      return currentPrime;
+    }
+
+    expect(findTheNthPrime(1)).toBe(2);
+    expect(findTheNthPrime(5)).toBe(11);
+    expect(findTheNthPrime(20)).toBe(71);
+    expect(findTheNthPrime(10001)).toBe(104743);
+  });
 });
